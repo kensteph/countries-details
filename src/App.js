2b014077ba7f6210d65da51f314c8bb5a6bd761d
Kender ROMAIN <kenderromain@gmail.com>
2023-03-12 20:07:48 -0400

    Create RootLayout.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import Header from './components/Header';
-import Home from './pages/Home';
-
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
