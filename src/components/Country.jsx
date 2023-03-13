4604aaf81c039381aad8a57cdfecf927987074e5
Kender ROMAIN <kenderromain@gmail.com>
2023-03-12 20:14:36 -0400

    Implement Routes for the app.

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Country = ({ className, country }) => (
   <div className={`countryCard ${className}`}>
-    <span className="material-symbols-outlined floatIcon">
-      arrow_circle_right
-    </span>
-    <img className="countryCardImg" src={country.flag} alt="Flag" />
-    <article className="countryInfo">
-      <span className="country">{country.name}</span>
-      <span className="nbViews">11,000</span>
-    </article>
+    <Link to="/details">
+      <span className="material-symbols-outlined floatIcon">
+        arrow_circle_right
+      </span>
+      <img className="countryCardImg" src={country.flag} alt="Flag" />
+      <article className="countryInfo">
+        <span className="country">{country.name}</span>
+        <span className="nbViews">11,000</span>
+      </article>
+    </Link>
   </div>
 );
 
--- src/components/MostSearch.jsx
+++ src/components/MostSearch.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MostSearch = () => (
-  <div className="mostSearchContainer">
-    <img className="flag" src="https://flagcdn.com/w320/bw.png" alt="Flag" />
-    <article className="searchStats">
-      <span className="country">Botswana</span>
-      <span className="nbViews">3 views</span>
-    </article>
-  </div>
+  <Link to="/details">
+    <div className="mostSearchContainer">
+      <img className="flag" src="https://flagcdn.com/w320/bw.png" alt="Flag" />
+      <article className="searchStats">
+        <span className="country">Botswana</span>
+        <span className="nbViews">3 views</span>
+      </article>
+    </div>
+  </Link>
 );
 
 export default MostSearch;
